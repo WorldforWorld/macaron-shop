2c7ffc4cb7a341de97f2139ed2d77452de286f58
Yuri Kuzmichev <kuzmichev@it-tian.ru>
2025-04-22 15:52:44 +0500

    update file

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -25,7 +25,7 @@ export const html = () => {
         })
       )
       .pipe(app.plugins.replace(/@img\//g, "img/"))
-      .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
+      // .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
       .pipe(
         app.plugins.if(
           app.isBuild,
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,3 @@
-import { burgerMenu } from "./templates/burgerMenu.js";
-import { mobileMenu } from "./templates/mobileMenu.js";
-
 document.addEventListener("DOMContentLoaded", () => {
   burgerMenu("burgerMenu", "navMenu");
   mobileMenu(".js-submenu .menu__select");
@@ -8,3 +5,36 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#navMenu").classList.remove("open");
   });
 });
+
+function mobileMenu(itemSelectors) {
+  const menuItems = document.querySelectorAll(itemSelectors);
+  if (!menuItems.length) return;
+  menuItems.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      const menuContent = button.nextElementSibling;
+      const isActive = button.classList.contains("active");
+      menuItems.forEach((btn) => {
+        btn.classList.remove("active");
+        btn.nextElementSibling.style.maxHeight = 0;
+      });
+      if (!isActive) {
+        button.classList.add("active");
+        menuContent.style.maxHeight = `${menuContent.scrollHeight}px`;
+      }
+    });
+  });
+}
+
+function burgerMenu(burgerSelector, navSelector) {
+  const burgerMenu = document.getElementById(burgerSelector);
+  const body = document.querySelector("body");
+  const navMenu = document.getElementById(navSelector);
+  if (!burgerMenu || !navMenu || !body) return;
+  burgerMenu.addEventListener("click", function () {
+    this.classList.toggle("open");
+    navMenu.classList.toggle("open");
+    body.classList.toggle("js-overflow");
+    this.setAttribute("aria-expanded", this.classList.contains("open"));
+  });
+}
